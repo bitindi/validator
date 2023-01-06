ed4c0a39d493efa5ee6fc3ca986ad3f5bf026f92
Kumar <119077822+kumarbitindi@users.noreply.github.com>
2023-01-06 11:12:17 -0600

    Update start_validator.sh

--- start_validator.sh
+++ start_validator.sh
@@ -4,7 +4,7 @@ NODEIP=$(curl -s4 icanhazip.com)
 
 # Create systemd Service File
 cd /etc/systemd/system
-echo "Starting Alvey service..."
+echo "Starting Bitindi service..."
 echo "
         [Unit]
         Description=Alvey Node Service
@@ -15,11 +15,11 @@ echo "
         User=$USER
         Group=$USER
         LimitNOFILE=4096
-        WorkingDirectory=/$USER/validator/alveychain
-        ExecStart=/$USER/validator/alveychain/alvey server --data-dir /$USER/validator/alveychain/data-dir  --chain /$USER/validator/genesis.json --grpc 0.0.0.0:10000 --libp2p 0.0.0.0:10001 --jsonrpc 0.0.0.0:10002 --nat $NODEIP --seal
+        WorkingDirectory=/$USER/validator/bitindichain
+        ExecStart=/$USER/validator/bitindichain/bitindi server --data-dir /$USER/validator/bitindichain/data-dir  --chain /$USER/validator/genesis.json --grpc 0.0.0.0:10000 --libp2p 0.0.0.0:10001 --jsonrpc 0.0.0.0:10002 --nat $NODEIP --seal
         [Install]
         WantedBy=multi-user.target
-" | sudo tee alvey.service
+" | sudo tee bitindi.service
 
 if grep -q ForwardToSyslog=yes "/etc/systemd/journald.conf"; then
   sudo sed -i '/#ForwardToSyslog=yes/c\ForwardToSyslog=no' /etc/systemd/journald.conf
@@ -33,6 +33,6 @@ echo
 # Start systemd Service
 sudo systemctl force-reload systemd-journald
 sudo systemctl daemon-reload
-sudo systemctl start alvey.service
+sudo systemctl start bitindi.service
 
 read -n 1 -s -r -p "Service successfully started! Press any key to continue..."
